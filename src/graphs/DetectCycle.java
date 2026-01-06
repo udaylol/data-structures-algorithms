661739aabea318ab400e2ff8a20b62b1ce28dc59
Uday <157674103+udaylol@users.noreply.github.com>
2026-01-07 04:03:24 +0530

    detect cycle in graph

---
+++ src/graphs/DetectCycle.java
@@ -0,0 +1,43 @@
+package graphs;
+
+import java.util.List;
+
+public class DetectCycle {
+    static boolean[] visited;
+
+    static boolean detectCycle(List<List<Integer>> graph) {
+        int v = graph.size();
+        visited = new boolean[v];
+
+        for (int i = 0; i < v; i++) {
+            if (!visited[i]) {
+                if (dfs(graph, i, -1)) return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean dfs(List<List<Integer>> graph, int node, int parent) {
+        visited[node] = true;
+
+        for (int neighbor : graph.get(node)) {
+            if (!visited[neighbor]) {
+                if (dfs(graph, neighbor, node)) return true;
+            } else if (neighbor != parent) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> graph = Graph.input();
+        boolean cycle = detectCycle(graph);
+
+        if (cycle) {
+            System.out.println("Cyclic Undirected Graph");
+        } else {
+            System.out.println("Acyclic Undirected Graph");
+        }
+    }
+}
